94feeb323d466c5bae64131bae86e19d44e26556
melanie2016 <mel.lugones@gmail.com>
2018-05-06 20:24:58 -0300

    solucion del ejercicio 4

--- src/test/java/ar/edu/unlam/tallerweb1/probando/testeoGeneral.java
+++ src/test/java/ar/edu/unlam/tallerweb1/probando/testeoGeneral.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.probando.modelo.Barrio;
 import ar.edu.unlam.tallerweb1.probando.modelo.Direccion;
 import ar.edu.unlam.tallerweb1.probando.modelo.Farmacia;
 
@@ -21,7 +22,9 @@ public class testeoGeneral extends SpringTest{
         assertThat(getSession().isConnected()).isTrue();
 
 	}
-//2-​ ​Hacer​ ​con​ ​junit​ ​un​ ​test​ ​que​ ​busque​ ​todas​ ​las​ ​farmacias​ ​de​ ​turno​ ​los​ ​días​ ​martes.  	
+	
+	
+//	2- Hacer con junit un test que busque todas las farmacias de turno los d�as martes.� 	
 	@Test
 	@Transactional @Rollback(true)
 	@SuppressWarnings("unchecked")
@@ -50,10 +53,14 @@ public class testeoGeneral extends SpringTest{
 				.list();
 		
 		assertThat(list.size()).isEqualTo(2);
+		
+		for(Farmacia misE : list) {
+			System.out.println(misE.getNombre());
+		}
 
 	}
 	
-//3- Hacer​ ​con​ ​junit​ ​un​ ​test​ ​que​ ​busque​ ​todas​ ​las​ ​farmacias​ ​de​ ​una​ ​calle. 
+//	3- Hacer con junit un test que busque todas las farmacias de una calle.
 	
 	@Test
 	@Transactional @Rollback(true)
@@ -96,8 +103,69 @@ public class testeoGeneral extends SpringTest{
 		
 		assertThat(list.size()).isEqualTo(2);
 		
-		System.out.println(list.toString());
-
-	}
+		for(Farmacia misE : list) {
+			System.out.println(misE.getNombre());
+		}
 		
+	}
+	
+//	4- Hacer con junit un test que busque todas las farmacias de un barrio.
+	@Test
+	@Transactional @Rollback(true)
+	@SuppressWarnings("unchecked")
+	public void testQueBusqueTodasLasFarmaciasDeUnBarrio() {
+		
+		Barrio barrio1 = new Barrio();
+		barrio1.setNombre("San Justo");
+		
+		Barrio barrio2 = new Barrio();
+		barrio2.setNombre("Boedo");
+		
+		Direccion direccion1= new Direccion();
+		direccion1.setCalle("Arieta");
+		direccion1.setBarrio(barrio1);
+		
+		Direccion direccion2= new Direccion();
+		direccion2.setCalle("Lugones");
+		direccion2.setBarrio(barrio2);
+		
+		Farmacia farmacia1= new Farmacia();
+		farmacia1.setNombre("Farmacity");
+		farmacia1.setDiaDeTurno("Martes");
+		farmacia1.setDireccion(direccion2);
+		
+		Farmacia farmacia2= new Farmacia();
+		farmacia2.setNombre("Farmaneta");
+		farmacia2.setDiaDeTurno("Miercoles");
+		farmacia2.setDireccion(direccion2);
+		
+		Farmacia farmacia3= new Farmacia();
+		farmacia3.setNombre("Central");
+		farmacia3.setDiaDeTurno("Martes");
+		farmacia3.setDireccion(direccion1);
+		
+		
+		getSession().save(barrio1);
+		getSession().save(barrio2);
+		getSession().save(direccion1);
+		getSession().save(direccion2);
+		getSession().save(farmacia1);
+		getSession().save(farmacia2);
+		getSession().save(farmacia3);
+		
+
+		List<Farmacia> list=
+				getSession().createCriteria(Farmacia.class)
+				.createAlias("direccion", "DireccionBuscada")
+				.createAlias("DireccionBuscada.barrio","BarrioBuscado")
+				.add(Restrictions.eq("BarrioBuscado.nombre", "Boedo"))
+				.list();
+		
+		assertThat(list.size()).isEqualTo(2);
+		
+		for(Farmacia misE : list) {
+			System.out.println(misE.getNombre());
+		}
+		
+	}	
 }
