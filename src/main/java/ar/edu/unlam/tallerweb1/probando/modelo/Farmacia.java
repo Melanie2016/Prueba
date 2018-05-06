3ffa9828878893a23b600516004122124e0cb2f5
melanie2016 <mel.lugones@gmail.com>
2018-05-06 18:14:53 -0300

    Se crean las clases Farmacia y Direccion

---
+++ src/main/java/ar/edu/unlam/tallerweb1/probando/modelo/Farmacia.java
@@ -0,0 +1,67 @@
+package ar.edu.unlam.tallerweb1.probando.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Farmacia{
+	
+	@Id 
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String telefono;
+	private String diaDeTurno;
+	
+	@ManyToOne
+	private Direccion direccion;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDiaDeTurno() {
+		return diaDeTurno;
+	}
+
+	public void setDiaDeTurno(String diaDeTurno) {
+		this.diaDeTurno = diaDeTurno;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	
+
+}
