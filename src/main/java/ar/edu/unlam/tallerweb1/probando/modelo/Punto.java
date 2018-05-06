3f85b951e4211980684181bc7c11e742d6314e29
esther alifraco <estheralifraco@gmail.com>
2018-05-06 18:44:25 -0300

    Creacion de clases Barrio y Comuna

--- src/main/java/ar/edu/unlam/tallerweb1/probando/modelo/Punto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/probando/modelo/Punto.java
@@ -1,5 +1,44 @@
 package ar.edu.unlam.tallerweb1.probando.modelo;
 
-public class Punto {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
 
+@Entity
+@Table(name = "PUNTO")
+public class Punto {
+	
+@Id
+@GeneratedValue(strategy= GenerationType.IDENTITY)
+@Column(name = "ID")
+private Long id;
+@Column(name = "LATITUD")
+private String latitud;
+@Column(name = "LONGITUD")
+private String longitud;
+
+public Long getId(){
+	return id;
 }
+public void setId(Long id){
+	this.id= id;
+}
+
+public String getLatitud(){
+	return latitud;
+}
+public void setLatitud(String latitud){
+	this.latitud= latitud;
+}
+
+public String getLongitud(){
+	return longitud;
+}
+public void setLongitud(String longitud){
+	this.longitud= longitud;
+}
+}
+
