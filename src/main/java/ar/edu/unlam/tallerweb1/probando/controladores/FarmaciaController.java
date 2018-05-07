9b5e9fee3854e0b769f3fa57172f80768f958aa5
melanie2016 <mel.lugones@gmail.com>
2018-05-06 21:31:55 -0300

    nuevo metodo redireccionar y vista redirect

--- src/main/java/ar/edu/unlam/tallerweb1/probando/controladores/FarmaciaController.java
+++ src/main/java/ar/edu/unlam/tallerweb1/probando/controladores/FarmaciaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 
@@ -14,20 +15,20 @@ public class FarmaciaController {
 	
 	//http://localhost:8080/Prueba/operacion/suma/operador1/4/operador2/5
 	@RequestMapping(value="operacion/{operacion}/operador1/{operador1}/operador2/{operador2}",method=RequestMethod.GET)
-	public ModelAndView irA (@PathVariable String operacion,@PathVariable Integer operador1,@PathVariable Integer operador2) {
+	public ModelAndView irACalcular (@PathVariable String operacion,@PathVariable Integer operador1,@PathVariable Integer operador2) {
 				
 		switch (operacion) {
 			case "sumar": 
 				ModelMap model = new ModelMap();
 				model.put("valor1", operador1);
 				model.put("valor2", operador2);
-				return new ModelAndView ("suma",model);
+				return new ModelAndView ("sumar",model);
 					
 			case "restar":
 				ModelMap model2 = new ModelMap();
 				model2.put("valor1", operador1);
 				model2.put("valor2", operador2);
-				return new ModelAndView ("resta",model2);
+				return new ModelAndView ("restar",model2);
 								
 			case "dividir":
 				if(operador2!=0) {
@@ -35,20 +36,20 @@ public class FarmaciaController {
 					model3.put("valor1", operador1);
 					model3.put("valor2", operador2);
 					
-					return new ModelAndView ("division",model3);
+					return new ModelAndView ("dividir",model3);
 				}else {
 					String msj2 = "El operador2 no debe ser 0";
 					ModelMap errorDiv = new ModelMap();
 					errorDiv.put("errorDiv", msj2);
 					
-					return new ModelAndView ("division",errorDiv);
+					return new ModelAndView ("dividir",errorDiv);
 				}
 				
 			case "multiplicar":
 				ModelMap model4 = new ModelMap();
 				model4.put("valor1", operador1);
 				model4.put("valor2", operador2);
-				return new ModelAndView ("multiplicacion",model4);
+				return new ModelAndView ("multiplicar",model4);
 			}
 				
 				
@@ -60,5 +61,20 @@ public class FarmaciaController {
 			return new ModelAndView ("error",modelError);
 	
 		}
+	
+	
+	@RequestMapping(path = "/redirect", method = RequestMethod.GET)
+	public ModelAndView irARedirect() {
+		ModelMap modelo = new ModelMap();
+		return new ModelAndView("redirect",modelo);
+	}
+	
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public ModelAndView redireccionar() {
+				
+		return new ModelAndView(new RedirectView("redirect") ) ; // (vista,mapa)
+
+	}
+		
 	
 }
